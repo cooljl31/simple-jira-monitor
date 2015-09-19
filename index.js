7ac57c171ab72566f219dcec12e1524b57c356dc
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2015-09-20 00:08:34 +0200

    Added Handlebars views

--- index.js
+++ index.js
@@ -2,18 +2,24 @@
 
 var express = require('express');
 var JiraApi = require('./lib/jira').JiraApi;
+var expressHbs = require('express-handlebars');
 var config = {
     jira: require('./config/jira.json'),
     server: require('./config/server.json')
 };
+
+// setup express app
 var app = express();
+app.set('views', './views');
+app.engine('hbs', expressHbs({extname:'hbs'}));
+app.set('view engine', 'hbs');
 
 var jira = new JiraApi('https', config.jira.host, config.jira.port, config.jira.user, config.jira.password, '2.0');
 
 app.get('/monitor/view/:view', function(req, res) {
     var viewId = req.params.view;
     jira.getBacklogForRapidView(viewId, function(error, data) {
-        res.send(data);
+        res.render('rapid-view-monitor',data);
     })
 
 });
